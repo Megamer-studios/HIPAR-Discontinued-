4dcbd17d181a36c395be4f96304bae3128dc27a9
MegamerStudios <cozoral@outlook.com>
2024-02-16 22:11:17 +0300

    Add project files.

---
+++ HIPAR/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace HIPAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ControlDark;
+            textBox1.Font = new Font("Public Pixel", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(12, 415);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(776, 22);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Maroon;
+            label1.Font = new Font("Public Pixel", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(26, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(195, 35);
+            label1.TabIndex = 3;
+            label1.Text = "HIPAR";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.DimGray;
+            label2.Font = new Font("Public Pixel", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(22, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(757, 60);
+            label2.TabIndex = 4;
+            label2.Text = "Welcome to HIPAR \r\n(Highly Intelligent Processing Arigtonyte Result).\r\nA Highly intelligent processing system based on\r\nThe Arygtonite Framework\r\n";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 64, 64);
+            button1.Font = new Font("Public Pixel", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(683, 379);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 30);
+            button1.TabIndex = 5;
+            button1.Text = "SEND";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoValidate = AutoValidate.EnablePreventFocusChange;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            ControlBox = false;
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ HIPAR/Form1.cs
@@ -0,0 +1,138 @@
+namespace HIPAR
+{
+    public partial class Form1 : Form
+    {
+        int prog = 0;
+        // 0 = basic
+        // 1 = first process
+        // -1 = GTFO
+        // 3 = search
+        // Arigtonyte
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // STANDARTS --
+            if (prog == 1)
+            {
+                label2.Text = ("What process would you like to start?");
+                textBox1.Text = ("");
+                prog = 2;
+            }
+            if (textBox1.Text == ("exit"))
+            {
+                this.Close();
+            }
+
+            if (prog == 0)
+            {
+                if (textBox1.Text == ("start"))
+                {
+
+                    label2.Text = ("Starting a process.\n Is that what you desire?\n (Y/N)");
+                    textBox1.Text = ("");
+                    prog = 1;
+
+
+                }
+            }
+            // --
+            if (prog == 1)
+            {
+                if (textBox1.Text == ("Y") || textBox1.Text  == ("y"))
+                {
+
+                    label2.Text = ("What process would you like to start?");
+                    textBox1.Text = ("");
+                    prog = 2;
+
+
+                }
+
+            }
+            if (prog == 1)
+            {
+                if (textBox1.Text == ("N") || textBox1.Text == ("n"))
+                {
+
+                    label2.Text = ("GTFO then...");
+                    textBox1.Text = ("");
+                    prog = -1;
+
+
+                }
+
+            }
+            if (prog == 2)
+            {
+                if (textBox1.Text == ("process: search"))
+                {
+
+                    label2.Text = ("What would you like to search for?");
+                    textBox1.Text = ("");
+
+
+
+                }
+
+            }
+            if (prog == 3)
+            {
+
+
+                    label2.Text = ("Knowledgee on that topic \ndoesn't seem to be on our database. \n Check out The Arigtonyte Framework databse\n for more information on the search process.");
+                    textBox1.Text = ("");
+
+                    prog = 1;
+
+
+                
+
+            }
+            // DELAY MAKERS --
+
+            if (label2.Text == ("What would you like to search for?"))
+            {
+                prog = 3;
+            }
+            if (label2.Text == ("Knowledgee on that topic \ndoesn't seem to be on our database. \n Check out The Arigtonyte Framework databse\n for more information on the search process."))
+            {
+                prog = 1;
+            }
+            // --
+
+        }
+    }
+}
